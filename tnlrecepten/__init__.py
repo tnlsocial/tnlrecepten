5a457f5bd8205a100878388fd8a84277cfe89154
semyon <github@semyon.eu>
2024-03-13 11:17:07 +0100

    revert local db usage

--- tnlrecepten/__init__.py
+++ tnlrecepten/__init__.py
@@ -12,9 +12,9 @@ def create_app():
     bootstrap = Bootstrap5(app)
 
     csrf.init_app(app)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/tnlrecepten/db/recepten.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/tnlrecepten/db/recepten.db'
     # Local development
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recepten.db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recepten.db'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['SECRET_KEY'] = 'thisisadummyvaluereplaceitwithsomethinglikeanenvvar!'
     app.config['BOOTSTRAP_SERVE_LOCAL'] = True
